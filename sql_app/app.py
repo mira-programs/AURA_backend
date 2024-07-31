8e2ae26b2f27c0214153efeb23a0fba962bffcfd
mariaaeid <mariaeid20@gmail.com>
2024-07-31 12:25:55 +0300

    Create app.py

---
+++ sql_app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+import tensorflow as tf
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
+from tensorflow.keras.preprocessing import image
+import numpy as np
+
+app = Flask(__name__)
+model = MobileNetV2(weights='imagenet')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    file = request.files['file']
+    img = image.load_img(file, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    preds = model.predict(x)
+    results = decode_predictions(preds, top=1)[0]
+    return jsonify({ 'predictions': results })
+
+if __name__ == '__main__':
+    app.run(debug=True)
