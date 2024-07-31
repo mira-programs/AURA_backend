a939452e78e2cfaaab33d637ca5349e038c91a57
mariaaeid <mariaeid20@gmail.com>
2024-07-31 14:52:16 +0300

    Update app.py

--- sql_app/app.py
+++ sql_app/app.py
@@ -1,3 +1,5 @@
+# loading the AI model using flask
+
 from flask import Flask, request, jsonify
 import tensorflow as tf
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
