e7c2a69a80cbe438c1bb8d0a31e89c450a6967fd
mira-programs <mirahussein06@gmail.com>
2024-08-01 15:56:52 +0300

    resotred db
    brought back db

---
+++ database_setup.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE aura;
+
+USE aura;
+
+CREATE TABLE challenges (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    description VARCHAR(1000),
+    points INT
+);
+
+CREATE TABLE accounts (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,  
+    points INT DEFAULT 0,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100)
+);
+
+CREATE TABLE challenge_status (
+    account_id INT,
+    challenge_id INT,
+    completed BOOLEAN DEFAULT FALSE,
+    failed BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY(account_id, challenge_id),
+    FOREIGN KEY(account_id) REFERENCES accounts(id),
+    FOREIGN KEY(challenge_id) REFERENCES challenges(id)
+);
+
+CREATE TABLE friends (
+    account_id INT,
+    friend_id INT,
+    PRIMARY KEY(account_id, friend_id),
+    FOREIGN KEY(account_id) REFERENCES accounts(id),
+    FOREIGN KEY(friend_id) REFERENCES accounts(id)
+);
